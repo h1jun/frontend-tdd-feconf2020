cda3a6fb331e59d43e2f19a7b2fa79c3bbf455d1
Wonjun <dittoavec@gmail.com>
2022-02-28 12:34:55 +0900

    Add list 컴포넌트 분리 및 테스트 코드

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import List from "./List";
 
 export default function App() {
+  const tasks = [
+    { id: 1, title: "아무 일도 하기 싫다" },
+    { id: 2, title: "건물 매입" },
+  ];
   return (
     <div>
       <h1>To-do</h1>
+      <List tasks={tasks} />
     </div>
   );
 }
