1b4394ebfe7f03428fd98a8c971dfeb1c95848f6
Wonjun <dittoavec@gmail.com>
2022-02-28 14:49:51 +0900

    BDD 기반의 테스트 코드 작성

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,12 +4,32 @@ import { render } from "@testing-library/react";
 
 import List from "./List";
 
-import tasks from "../fixtures/tasks";
+// with tasks
+// renders tasks
+
+// without tasks
+// renders no tasks message
 
 describe("List", () => {
-  it("renders tasks", () => {
-    const { container } = render(<List tasks={tasks} />);
-    expect(container).toHaveTextContent("아무 일도 하기 싫다");
-    expect(container).toHaveTextContent("건물 매입");
+  context("with tasks", () => {
+    const tasks = [
+      { id: 1, title: "아무 일도 하기 싫다" },
+      { id: 2, title: "건물 매입" },
+    ];
+
+    it("renders tasks", () => {
+      const { container } = render(<List tasks={tasks} />);
+      expect(container).toHaveTextContent("아무 일도 하기 싫다");
+      expect(container).toHaveTextContent("건물 매입");
+    });
+  });
+
+  context("without tasks", () => {
+    const tasks = [];
+
+    it("renders no task message", () => {
+      const { container } = render(<List tasks={tasks} />);
+      expect(container).toHaveTextContent("할 일이 없어요!");
+    });
   });
 });
